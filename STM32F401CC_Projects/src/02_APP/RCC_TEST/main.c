d34bbfb22c137bb08fa5fcd008048e03e2ea12bf
Aly Mahmoud <aly.ma7moud6@gmail.com>
2024-03-11 12:21:29 +0200

    PLATFORM I/O ADDED + (RCC): DEBUGGING

---
+++ STM32F401CC_Projects/src/02_APP/RCC_TEST/main.c
@@ -0,0 +1,22 @@
+#include "00_MCAL/RCC/STM32F401cc_HAL_RCC.h"
+
+/*Testing RCC
+
+int main ()
+{
+    RCC_EnableClock				( CLK_HSI );
+    RCC_EnableClock				( CLK_HSE );
+    RCC_EnableClock				( CLK_PLL );
+    RCC_EnableClock             ( CLK_PLLI2S );
+    
+
+    while (1)
+    {
+
+    };
+
+    return 0;
+}
+
+*/
+
